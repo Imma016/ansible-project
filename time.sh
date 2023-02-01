53359d23872ccfd3535585312a85746bf3c0a588
Cloud User <centos@ip-172-31-54-109.ec2.internal>
2023-02-01 01:02:16 +0000

    updating

---
+++ time.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+echo `date`
+
+year=`date +%y`
+month=`date +%m`
+day=`date +%d`
+
+hour=`date +%H`
+minute=`date +%M`
+second=`date +%S`
+
+echo "current Date is : $day/$month/$year"
+echo "current Time is : $hour:$minute:$second"
+
+
